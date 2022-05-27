06ad4dd2b8be2f82925fbc79497a7b99be659417
NathDLR <86125084+NathDLR@users.noreply.github.com>
2022-05-27 21:33:01 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,25 +1,51 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
 
 // Next/previous controls
-function plusSlides(n, sliderId) {
-    showSlides(slideIndex += n, sliderId);
+function plusSlides1(n, sliderId) {
+    showSlides1(slideIndex1 += n, sliderId);
 }
 
 // Thumbnail image controls
-function currentSlide(n, sliderId) {
-    showSlides(slideIndex = n, sliderId);
+function currentSlide1(n, sliderId) {
+    showSlides1(slideIndex1 = n, sliderId);
 }
 
-function showSlides(n, sliderId) {
+function showSlides1(n, sliderId) {
     let i;
-    let slides = document.getElementsByClassName("mySlides_" + sliderId);
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("mySlides_1");
+    if (n > slides.length) { slideIndex1 = 1}
+    if (n < 1) { slideIndex1 = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        slides[slideIndex-1].classList.remove("show");
+        slides[slideIndex1-1].classList.remove("show");
     }
-    slides[slideIndex-1].style.display = "block";
-    slides[slideIndex-1].classList.add("show");
+    slides[slideIndex1-1].style.display = "block";
+    slides[slideIndex1-1].classList.add("show");
+}
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n, sliderId) {
+    showSlides2(slideIndex2 += n, sliderId);
+}
+
+// Thumbnail image controls
+function currentSlide2(n, sliderId) {
+    showSlides2(slideIndex2 = n, sliderId);
+}
+
+function showSlides2(n, sliderId) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides_2");
+    if (n > slides.length) { slideIndex2 = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[slideIndex2 - 1].classList.remove("show");
+    }
+    slides[slideIndex2 - 1].style.display = "block";
+    slides[slideIndex2 - 1].classList.add("show");
 }
